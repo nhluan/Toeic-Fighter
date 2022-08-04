589c0d63f6b6eb2896065f6fe8cf62f41f1c8fda
Loc My Tho <lapdoan.010102@gmail.com>
2022-08-04 14:52:49 +0700

    thêm trang chọn dề thi

--- Source/ToeciExam/src/app/controllers/testcontroller.js
+++ Source/ToeciExam/src/app/controllers/testcontroller.js
@@ -1,7 +1,6 @@
 const Test = require("../models/test.model");
-const part5 = require("../models/part5.model");
+const part5Model = require("../models/part5.model");
 const part1Model = require("../models/part1.model");
-const part5Answer = require("../models/part5Answer.model");
 
 class testController {
   showTest(req, res, next) {
@@ -19,7 +18,7 @@ class testController {
           console.log(err);
           res.render("error404");
         }
-        res.render("test", { tests });
+        res.render("choosetest", { tests });
       });
     } else res.render("error404");
   }
@@ -30,7 +29,6 @@ class testController {
     var test = req.params.test;
 
     if (part == 1) {
-      console.log("\n\n\nPART1\n\n\n");
       part1Model.find({ test }, function (err, parttest) {
         if (err) {
           console.log("loix 1");
@@ -40,30 +38,33 @@ class testController {
         return res.render("part1_test", { parttest });
         //       return res.json(parttest);
       });
+
     } else if (part == 2) {
       res.render("part2_test", { test: test });
+
     } else if (part == 3) {
       res.render("part3_test", { test: test });
+
     } else if (part == 4) {
       res.render("part4_test", { test: test });
+
     } else if (part == 5) {
-      part5.find({ test }, function (err, parttest) {
+      part5Model.find({ test }, function (err, parttest) {
         if (err) {
           console.log("loix 1");
           console.log(err);
           res.render("test", { layout: false });
-          // res.json(parttest);
         }
         return res.render("part5_test", { parttest });
       });
+
     } else if (part == 6) {
       res.render("part6_test", { test: test });
+
     } else if (part == 7) {
       res.render("part7_test", { test: test });
     }
-  }
-  markExam(req, res, next) {
-    // chấm điểm part ? test ?
+    else res.render("error404")
   }
 }
 
