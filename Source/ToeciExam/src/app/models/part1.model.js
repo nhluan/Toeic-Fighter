855908cedfae09fdd3d24f5c924fc471ad2d8dca
Duc <caonhatduc9@gmail.com>
2022-08-04 16:01:21 +0700

    sua schema part 1

--- Source/ToeciExam/src/app/models/part1.model.js
+++ Source/ToeciExam/src/app/models/part1.model.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const part1Schema = new Schema({
-  num: String,
-  Q: String,
+  numTest: String,
+  numQ: String,
   audioLink: String,
   imageLink: String,
   Answer: {
